8b8d703cb32f7a606c322adabc428f4ec9a3a59b
Klára Tauchmanová <klara.tauchmanova@gmail.com>
2022-09-22 21:43:34 +0200

    feat: class filling in the best word form into the sentence.
    Co-authored-by: b0r3k <b0r3k@users.noreply.github.com>

--- cilf.py
+++ cilf.py
@@ -1,74 +1,77 @@
-from transformers import TFAutoModelForMaskedLM, AutoTokenizer
-import numpy as np
 from collections import defaultdict
+from glob import glob
+from json import load
 from morph_api import MorphoDiTa
+import numpy as np
+from transformers import TFAutoModelForMaskedLM, AutoTokenizer
+from pathlib import Path
 
-model_checkpoint = "ufal/robeczech-base"
-model = TFAutoModelForMaskedLM.from_pretrained(model_checkpoint)
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+class Editor:
+    def __init__(self):
+        self.lexicons = self._load_lexicons()
+        self.morphodita = MorphoDiTa()
+        
+        model_checkpoint = "ufal/robeczech-base"
+        self.model = TFAutoModelForMaskedLM.from_pretrained(model_checkpoint)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
+    def _load_lexicons(self):
+        """ Load lexicons from the lexicons directory. """
+        lexicons = {}
+        for f_name in glob('lexicons/*.json'):
+            with open(f_name, 'r') as json_f:
+                lexicons[Path(f_name).stem] = load(json_f)
+        return lexicons
 
-while True:
-    text = input("Text:\t")
-    word = input("Word:\t")
-    #pos = input("POS:\t")
-    
-    #pos = "A"
-    #text = "Tohle je [MASK] hračka než hrnec ."
-    #text = "Tohle je [MASK] hračka ."
-    inputs = tokenizer(text, return_tensors="np")
-    token_logits = model(**inputs).logits
-    # Find the location of [MASK] and extract its logits
-    mask_token_index = np.argwhere(inputs["input_ids"] == tokenizer.mask_token_id)[0, 1]
-    mask_token_logits = token_logits[0, mask_token_index, :]
-    # Pick the [MASK] candidates with the highest logits
-    # We negate the array before argsort to get the largest, not the smallest, logits
-    top_10_tokens = np.argsort(-mask_token_logits)[:50].tolist()
+    def template(self, template, word):
+        """
+        Fill the template with the given word in the correct form.
 
-    tokens = text.split()
-    mask_index = tokens.index("[MASK]")
-    print(mask_index)
-    sentences = []
-    for result in top_10_tokens:
-        tokens[mask_index] = tokenizer.decode([result]).strip()
-        sentences.append(" ".join(tokens))
-    sentences_joined = "\n".join(sentences)
+        Parameters:
+            template (str): str with one [MASK] token
+            word (str): word to be masked
+        
+        Returns:
+            str: template with [MASK] replaced by word in correct form
+        """
+        # Get the possible forms of the word
+        generated = self.morphodita.generate(word)[0]
+        tag_words = defaultdict(list)
+        for gen in generated:
+            tag_words[gen["tag"]].append(gen["form"])
 
-    mt = MorphoDiTa()
-    generated = mt.generate(word)[0]
-    tag_words = defaultdict(list)
-    for gen in generated:
-        tag_words[gen["tag"]].append(gen["form"])
+        # Use language model to predict words suitable for given context
+        inputs = self.tokenizer(template, return_tensors="np")
+        token_logits = self.model(**inputs).logits
+        # Find the location of [MASK] and extract its logits
+        mask_token_index = np.argwhere(inputs["input_ids"] == self.tokenizer.mask_token_id)[0, 1]
+        mask_token_logits = token_logits[0, mask_token_index, :]
+        # Pick the [MASK] candidates with the highest logits
+        # We negate the array before argsort to get the largest, not the smallest, logits
+        top_tokens = np.argsort(-mask_token_logits)[:50].tolist()
+        tokens = template.split()
+        mask_index = tokens.index("[MASK]")
+        sentences = []
+        for result in top_tokens:
+            tokens[mask_index] = self.tokenizer.decode([result]).strip()
+            sentences.append(" ".join(tokens))
+        sentences_joined = "\n".join(sentences)
 
-    tagged = mt.tag(sentences_joined)
-    tags = []
-    for sentence in tagged:
-        tag = sentence[mask_index]["tag"]
-        if tag in tag_words:
-            tags.append(tag)
-            print(tag, "\t", sentence[mask_index]["token"])
-    
-    # Find the most frequent tag in a tag list
-    most_freq_tag = max(set(tags), key = tags.count)
-    print(tag_words[most_freq_tag])
-    # results = set()
-    # for tag in tags:
-    #     results.update(tag_words[tag])
-    
-    # print(results)
+        # Get the possible tags from predicted words
+        tagged = self.morphodita.tag(sentences_joined)
+        tags = []
+        for sentence in tagged:
+            tag = sentence[mask_index]["tag"]
+            if tag in tag_words:
+                tags.append(tag)
+                # print(tag, "\t", sentence[mask_index]["token"])
+        
+        # Find the most frequent tag in a tag list
+        most_freq_tag = max(set(tags), key = tags.count)
+        for form in tag_words[most_freq_tag]:
+            print(template.replace("[MASK]", form))
 
-"""
-POS:
-A - adjektivum (přídavné jméno) 
-C - numerál (číslovka, nebo číselný výraz s číslicemi) 
-D - adverbium (příslovce) 
-I - interjekce (citoslovce) 
-J - konjunkce (spojka) 
-N - substantivum (podstatné jméno) 
-P - pronomen (zájmeno) 
-R - prepozice (předložka) 
-T - partikule (částice) 
-V - verbum (sloveso) 
-X - neznámý, neurčený, neurčitelný slovní druh 
-Z - interpunkce, hranice věty
-"""
+if __name__ == "__main__":
+    e = Editor()
+    print(e.lexicons.keys())
+    e.template("V [MASK] je krásně.", "příroda")
