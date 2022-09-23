428c3736841ca056a745350e041893ce78d60873
Klára Tauchmanová <klara.tauchmanova@gmail.com>
2022-09-23 11:46:11 +0200

    feat: lemmatize color names after scraping.
    Co-authored-by: b0r3k <b0r3k@users.noreply.github.com>

--- lexicons/retrieval/wikipedia_scraper.py
+++ lexicons/retrieval/wikipedia_scraper.py
@@ -1,6 +1,8 @@
 import pandas as pd 
-import requests 
+import requests
 from bs4 import BeautifulSoup
+
+from cilf_robeczech.morph_api import MorphoDiTa
 
 colors_url = "https://cs.wikipedia.org/wiki/Seznam_barev"
 cities_url = "https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku_podle_po%C4%8Dtu_obyvatel"
@@ -9,6 +11,7 @@ months_url = "https://cs.wikipedia.org/wiki/Kalend%C3%A1%C5%99n%C3%AD_m%C4%9Bs%C
 class WikiScraper:
     def __init__(self, url: str) -> None:
         """Scrape table from Wikipedia page into a Pandas Dataframe."""
+        self.morphodita = MorphoDiTa()
         # Sent GET request to the Wikipedia URL
         response = requests.get(url)
 
@@ -25,12 +28,17 @@ class WikiScraper:
         self.df = self.df[column_name]
         self.df = self.df.loc[~self.df.str.contains(" ")]
 
+    def lemmatize(self) -> None:
+        """Lemmatize the table."""
+        self.df = self.df.apply(self.morphodita.lemmatize)
+
     def save_table(self, json_name: str) -> None:
         """Save the table to a JSON file."""
         self.df.to_json(json_name, orient="records", force_ascii=False, indent=4)
 
 ws = WikiScraper(colors_url)
 ws.filter_data("Barva")
+ws.lemmatize()
 ws.save_table("lexicons/colors.json")
 
 ws = WikiScraper(cities_url)
