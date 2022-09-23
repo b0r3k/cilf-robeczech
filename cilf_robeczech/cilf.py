16de5bfb5987ace5901842fed287d12c780fb62f
Borek Požár <pozar.borek@gmail.com>
2022-09-23 19:40:13 +0200

    refactor: add comments, remove old code.
    Co-authored-by: AkralK  <AkralK@users.noreply.github.com>

--- cilf_robeczech/cilf.py
+++ cilf_robeczech/cilf.py
@@ -8,6 +8,11 @@ from pathlib import Path
 
 class Editor:
     def __init__(self):
+        """
+        Load the RobeCzech model, download from HF if necessary. 
+        Load all the lexicons into memory.
+        Create a class to simplify the communication with MorphoDiTa API.
+        """
         self.lexicons = self._load_lexicons()
         self.morphodita = MorphoDiTa()
         
@@ -16,7 +21,9 @@ class Editor:
         self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
     def _load_lexicons(self):
-        """ Load lexicons from the lexicons directory. """
+        """ 
+        Load all lexicons from the lexicons directory into memory. 
+        """
         lexicons = {}
         for f_name in glob('lexicons/*.json'):
             with open(f_name, 'r') as json_f:
@@ -25,33 +32,42 @@ class Editor:
 
     def template(self, template, words = None, iterations = 1):
         """
-        TODO: Add description
+        Fill in a template. The template contains "[FORMAT]", "[MASK]", or one of the lexicon tokens.
+        [FORMAT] - list of words to fill in the template must by provided in the `words` argument.
+        The words are put into the correct word form.
+        [MASK] - words to fill in the template are suggested using the MLM. Number of suggestions 
+        can be specified using the `iterations` argument.
+        [LEXICON] - random words are chosen from the "LEXICON" lexicon (if available) and filled 
+        into the template in a correct form. Number of suggestions can again be specified using the 
+        `iterations` argument.
+        
         Parameters:
-            template (str): str with one [MASK] token
-            words (List(str)): list of word to be filled in the template
-            iterations (int): number of iterations to perform
+            template (str): str with one "[FORMAT]", "[MASK]", or [LEXICON] token
+            words (List(str)): list of words to be filled in the template
+            iterations (int): number of suggestions to return
         
         Returns:
-            List(str): templats with [MASK] replaced by word in correct form iteration-times
+            List(str): templates with the [TOKEN] replaced by word in correct form of length `iterations`.
         """
         tokens = template.split()
         tokens_set = set(tokens)
         word_forms = []
 
-        # Correct word form
+        # Correct word form requested
         if "[FORMAT]" in tokens_set:
             mask_index = tokens.index("[FORMAT]")
             tokens[mask_index] = "[MASK]"
             template = " ".join(tokens)
             for w in words:
                 word_forms.append(self._get_correct_word_form(w, template))
-            # word_forms = self._get_correct_word_form(words, template)
-        # Language model suggestions
+
+        # Language model suggestions requested
         elif "[MASK]" in tokens_set:
             words = None
             template = template.strip()
             word_forms = self._generate_suggestions(template, suggestions_count = iterations)
-        # Lexicon lookup
+
+        # Lexicon lookup requested
         else:
             lexicon_type = ""
             words = None
@@ -88,20 +104,15 @@ class Editor:
             random_words = np.random.choice(self.lexicons[lexicon_type], iterations)
             for w in random_words:
                 word_forms.append(self._get_correct_word_form(w, template))
-            # word_forms = self._get_correct_word_form(random_word, template)
         
-        # if word_forms == None:
-        #     if words != None:
-        #         print(f"Word {words} cannot be found in given context.")
-        #     return
-        
+        # Fill in the template
         for form in word_forms:
             tokens[mask_index] = form
             print(" ".join(tokens))
 
     def _generate_suggestions(self, sentence, suggestions_count = 50):
         """
-        Generate suggestions for a given sentence using the language model.
+        Generate suggestions for a given sentence using the masked language model.
         
         Parameters:
             sentence (str): sentence containing [MASK] token
@@ -157,7 +168,6 @@ class Editor:
         mask_index = tokens.index("[MASK]")
         sentences = []
         for result in top_tokens:
-            # tokens[mask_index] = self.tokenizer.decode([result]).strip()
             tokens[mask_index] = result
             sentences.append(" ".join(tokens))
         sentences_joined = "\n".join(sentences)
@@ -178,56 +188,8 @@ class Editor:
         most_freq_tag = max(set(tags), key = tags.count)
         return tag_words[most_freq_tag][0]
 
-# while True:
-#     # text = input("Text:\t")
-#     # word = input("Word:\t")
-    
-#     model_checkpoint = "ufal/robeczech-base"
-#     model = TFAutoModelForMaskedLM.from_pretrained
-#     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-
-#     text = "Tohle je [MASK] hračka než [MASK] ."
-#     inputs = tokenizer(text, return_tensors="np")
-#     token_logits = model(**inputs).logits
-#     outputs = model(**inputs)
-#     predictions = outputs[0]
-#     sorted_preds, sorted_idx = predictions[0].sort(dim=-1, descending=True)
-#     for k in range(10):
-#         predicted_index = [sorted_idx[i, k].item() for i in range(0,24)]
-#         predicted_token = [tokenizer.convert_ids_to_tokens([predicted_index[x]])[0] for x in range(1,24)]
-#         print(predicted_token)
-#         break
-
-#     text = "Mám rád [MASK] [MASK] ."
-#     inputs = tokenizer(text, return_tensors="np")
-#     token_logits = model(**inputs).logits
-#     # Find the location of [MASK] and extract its logits
-#     mask_token_indices = np.argwhere(inputs["input_ids"] == tokenizer.mask_token_id)[:, 1]
-#     mask_token_logits = [token_logits[0, mask_token_index, :] for mask_token_index in mask_token_indices]
-#     # Pick the [MASK] candidates with the highest logits
-#     # We negate the array before argsort to get the largest, not the smallest, logits
-#     for result0, result1 in zip(mask_token_logits[0], mask_token_logits[1]):
-#         print(tokenizer.decode([result0]).strip(), tokenizer.decode([result1]).strip())
-#     #top_10_tokens = [np.argsort(-mask_token_logit)[:50].tolist() for mask_token_logit in mask_token_logits]
-#     #for result0, result1 in zip(top_10_tokens[0], top_10_tokens[1]):
-#     #    print(tokenizer.decode([result0]).strip(), tokenizer.decode([result1]).strip())
-
-#     # mask_token_index = np.argwhere(inputs["input_ids"] == tokenizer.mask_token_id)[0, 1]
-#     # mask_token_logits = token_logits[0, mask_token_index, :]
-#     # # Pick the [MASK] candidates with the highest logits
-#     # # We negate the array before argsort to get the largest, not the smallest, logits
-#     # top_10_tokens = np.argsort(-mask_token_logits)[:50].tolist()
-#     # for result in top_10_tokens:
-#     #     print(tokenizer.decode([result]).strip())
-
-
-#     # from transformers import pipeline
-#     # mask_filler = pipeline("fill-mask", model=model, tokenizer=tokenizer)
-#     # mask_filler("Tohle je [MASK].")
-
 if __name__ == "__main__":
     e = Editor()
-    # print(e.lexicons.keys())
     e.template(" V [FORMAT] je krásně .", "příroda")
     for _ in range(1):
         e.template(" Bez [ŽENSKÉ_JMÉNO] by se nám to dnes nepodařilo .")
