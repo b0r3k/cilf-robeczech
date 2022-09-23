5ea855dc4d420a0165a2cd147a8c468aa7668621
Borek Požár <pozar.borek@gmail.com>
2022-09-23 18:05:33 +0200

    feat: separate word format and word suggestion, allow multiple suggestions at once.
    Co-authored-by: AkralK  <AkralK@users.noreply.github.com>

--- cilf_robeczech/cilf.py
+++ cilf_robeczech/cilf.py
@@ -28,59 +28,76 @@ class Editor:
         TODO: Add description
         Parameters:
             template (str): str with one [MASK] token
-            word (List(str)): list of word to be filled in the template
+            words (List(str)): list of word to be filled in the template
+            iterations (int): number of iterations to perform
         
         Returns:
-            str: template with [MASK] replaced by word in correct form
+            List(str): templats with [MASK] replaced by word in correct form iteration-times
         """
         tokens = template.split()
+        tokens_set = set(tokens)
+        word_forms = []
 
         # Correct word form
-        if "[MASK]" in tokens:
-            word_forms = self._get_correct_word_form(words, template)
+        if "[FORMAT]" in tokens_set:
+            mask_index = tokens.index("[FORMAT]")
+            tokens[mask_index] = "[MASK]"
+            template = " ".join(tokens)
+            for w in words:
+                word_forms.append(self._get_correct_word_form(w, template))
+            # word_forms = self._get_correct_word_form(words, template)
+        # Language model suggestions
+        elif "[MASK]" in tokens_set:
+            words = None
+            template = template.strip()
+            word_forms = self._generate_suggestions(template, suggestions_count = iterations)
         # Lexicon lookup
         else:
             lexicon_type = ""
-            if "[DEN]" in tokens:
+            words = None
+            if "[DEN]" in tokens_set:
                 lexicon_type = "days"
                 mask_index = tokens.index("[DEN]")
-            elif "[MĚSÍC]" in tokens:
+            elif "[MĚSÍC]" in tokens_set:
                 lexicon_type = "months"
                 mask_index = tokens.index("[MĚSÍC]")
-            elif "[MUŽSKÉ_JMÉNO]" in tokens:
+            elif "[MUŽSKÉ_JMÉNO]" in tokens_set:
                 lexicon_type = "male_names"
                 mask_index = tokens.index("[MUŽSKÉ_JMÉNO]")
-            elif "[ŽENSKÉ_JMÉNO]" in tokens:
+            elif "[ŽENSKÉ_JMÉNO]" in tokens_set:
                 lexicon_type = "female_names"
                 mask_index = tokens.index("[ŽENSKÉ_JMÉNO]")
-            elif "[MĚSTO]" in tokens:
+            elif "[MĚSTO]" in tokens_set:
                 lexicon_type = "cities"
                 mask_index = tokens.index("[MĚSTO]")
-            elif "[BARVA]" in tokens:
+            elif "[BARVA]" in tokens_set:
                 lexicon_type = "colors"
                 mask_index = tokens.index("[BARVA]")
-            elif "[MUŽSKÉ_PŘÍJMENÍ]" in tokens:
+            elif "[MUŽSKÉ_PŘÍJMENÍ]" in tokens_set:
                 lexicon_type = "male_surnames"
                 mask_index = tokens.index("[MUŽSKÉ_PŘÍJMENÍ]")
-            elif "[ŽENSKÉ_PŘÍJMENÍ]" in tokens:
+            elif "[ŽENSKÉ_PŘÍJMENÍ]" in tokens_set:
                 lexicon_type = "female_surnames"
                 mask_index = tokens.index("[ŽENSKÉ_PŘÍJMENÍ]")
             else:
                 print("Unknown mask type.")
                 return
-            
-            random_word = np.random.choice(self.lexicons[lexicon_type])
+
             tokens[mask_index] = "[MASK]"
             template = " ".join(tokens)
-            word_forms = self._get_correct_word_form(random_word, template)
-           
-        if word_forms == None:
-            if words != None:
-                print(f"Word {words} cannot be found in given context.")
-            return
+            random_words = np.random.choice(self.lexicons[lexicon_type], iterations)
+            for w in random_words:
+                word_forms.append(self._get_correct_word_form(w, template))
+            # word_forms = self._get_correct_word_form(random_word, template)
+        
+        # if word_forms == None:
+        #     if words != None:
+        #         print(f"Word {words} cannot be found in given context.")
+        #     return
         
         for form in word_forms:
-            print(template.replace("[MASK]", form))
+            tokens[mask_index] = form
+            print(" ".join(tokens))
 
     def _generate_suggestions(self, sentence, suggestions_count = 50):
         """
@@ -93,7 +110,7 @@ class Editor:
         Returns:
             List(str): list of suggestions
         """
-        assert suggestions_count > 1, "suggestions_count must be greater than 1"
+        assert suggestions_count >= 1, "suggestions_count must be greater than 1"
         assert sentence.count("[MASK]") == 1, "Only one [MASK] token is allowed in the sentence."
 
         # Use language model to predict masked words
@@ -104,9 +121,10 @@ class Editor:
         mask_token_index = np.argwhere(inputs["input_ids"] == self.tokenizer.mask_token_id)[0, 1]
         mask_token_logits = token_logits[0, mask_token_index, :]
 
-        # Retunr the [MASK] candidates with the highest logits
+        # Return the [MASK] candidates with the highest logits
         # We negate the array before argsort to get the largest, not the smallest, logits
-        return np.argsort(-mask_token_logits)[:suggestions_count].tolist()
+        top = np.argsort(-mask_token_logits)[:suggestions_count].tolist()
+        return [self.tokenizer.decode([result]).strip() for result in top]
         
     def _get_correct_word_form(self, word, context):
         """
@@ -120,6 +138,9 @@ class Editor:
         Returns:
             List(str): list of correct forms of the word
         """
+        assert word != None, "word cannot be None"
+        assert context.count("[MASK]") == 1, "Only one [MASK] token is allowed in the sentence."
+
         # Get all the possible forms of the word
         generated = self.morphodita.generate(word)[0]
         if generated == []:
@@ -136,7 +157,8 @@ class Editor:
         mask_index = tokens.index("[MASK]")
         sentences = []
         for result in top_tokens:
-            tokens[mask_index] = self.tokenizer.decode([result]).strip()
+            # tokens[mask_index] = self.tokenizer.decode([result]).strip()
+            tokens[mask_index] = result
             sentences.append(" ".join(tokens))
         sentences_joined = "\n".join(sentences)
 
@@ -149,12 +171,12 @@ class Editor:
                 tags.append(tag)
         
         # Word cannot be found in the given context
-        if tags == []:
+        if not tags:
             return None
 
         # Find the most frequent tag in a tag list
         most_freq_tag = max(set(tags), key = tags.count)
-        return tag_words[most_freq_tag]
+        return tag_words[most_freq_tag][0]
 
 # while True:
 #     # text = input("Text:\t")
@@ -206,10 +228,10 @@ class Editor:
 if __name__ == "__main__":
     e = Editor()
     # print(e.lexicons.keys())
-    e.template("V [MASK] je krásně .", "příroda")
-    for _ in range(5):
-        e.template("Bez [ŽENSKÉ_JMÉNO] by se nám to dnes nepodařilo .")
-        e.template("[MUŽSKÉ_JMÉNO] se má výborně .")
+    e.template(" V [FORMAT] je krásně .", "příroda")
+    for _ in range(1):
+        e.template(" Bez [ŽENSKÉ_JMÉNO] by se nám to dnes nepodařilo .")
+        e.template(" [MUŽSKÉ_JMÉNO] se má výborně .")
         e.template(" Bydlí v [MĚSTO] .")
         e.template(" Přestěhoval se sem v [MĚSÍC] .")
-        e.template("V [MĚSÍC] bude krásné počasí .")
+        e.template(" Mám rád [MASK] .")
