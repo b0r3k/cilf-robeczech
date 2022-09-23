0c373d669b5d2984b5913cf027a97ae353c180c8
Klára Tauchmanová <klara.tauchmanova@gmail.com>
2022-09-23 22:58:32 +0200

    chore: provide more installation details.
    Co-authored-by: b0r3k <b0r3k@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cilf_robeczech',
+    version='1.0',
+    description='Conjugated/Inflected Lemma Filling using Robeczech',
+    author='Klára Tauchmanová & Borek Požár',
+    url='https://github.com/b0r3k/cilf-robeczech',
     packages = find_packages(),
 )
